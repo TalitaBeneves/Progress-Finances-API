c011d607846d40e47d52153f76b75762faf93ac6
Talita Beneves <talitabeneves15@gmail.com>
2023-12-16 23:50:31 -0300

    feat: adicionando username no template de email

--- Progress-Finances-API/Model/EnvioDeEmail.cs
+++ Progress-Finances-API/Model/EnvioDeEmail.cs
@@ -5,5 +5,7 @@
         public string EmailTo { get; set; }
         public string Subject { get; set; }
         public string Body { get; set; }
+        public string? Username { get; set; }
+        public string? Token{ get; set; }
     }
 }
